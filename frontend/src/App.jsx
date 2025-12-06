fd792cc039f7be78ca9b5116e4e20aaff5f5dba7
Pragathiswaran <mpragathiswaran@gmail.com>
2025-12-07 02:12:41 +0530

    feat: added button and input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Button from './component/common/Button'
+import Input from './component/common/Input'
 
 function App() {
 
   return (
     <>
       <Button label={"Hello"}></Button>
+      <Input placeholder={"Hello world!!!"}></Input>
     </>
   )
 }
