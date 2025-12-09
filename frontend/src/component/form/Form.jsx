9778eb110462ceb7a7cb4183c3ac1f30db93e3e2
Pragathiswaran <mpragathiswaran@gmail.com>
2025-12-09 20:33:29 +0530

    feat: created form wrapper

--- frontend/src/component/form/Form.jsx
+++ frontend/src/component/form/Form.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './../../styles/form.css'
 
 function Form() {
   return (
-    <div>Form</div>
+   <>
+    <div className='form-container'>
+      <h2>Sign Up</h2>
+      <form className='form'>
+        <div className='form-field-container'></div>
+        <div className='form-submit-container'></div>
+      </form>
+    </div>
+   </>
   )
 }
 
